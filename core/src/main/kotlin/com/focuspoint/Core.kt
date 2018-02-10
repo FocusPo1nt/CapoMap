09486a2c954a33d2b83d953311c018c69e502089
vova <newvovkka@gmail.com>
2018-02-10 17:46:58 +0300

    auth

--- core/src/main/kotlin/com/focuspoint/Core.kt
+++ core/src/main/kotlin/com/focuspoint/Core.kt
@@ -8,6 +8,8 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
 
 class Core {
     fun print() : String{
@@ -26,21 +28,41 @@ class Core {
         return "hello from Core!!1" + Single.just("1").blockingGet()
     }
 
-
-    fun auth() : Single<Response<ResponseBody>>{
+    fun auth(login : String, password: String, key : String) : Single<Response<ResponseBody>>{
         val retrofit : Retrofit = Retrofit.Builder()
-                .baseUrl("http://randomus.gq/")
+                .baseUrl("http://randomus.gq:8000/")
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
 
+        val service : TestInterface = retrofit.create(TestInterface::class.java)
+
+        println("login = admin")
+        val sum =  password +  key
+
+        println("password + key = " + sum)
+        println("sagrade = " + hashString("SHA-256", sum))
+
+        val loginJSON = JsonObject()
+        loginJSON.addProperty("login", login)
+        loginJSON.addProperty("sagrade", hashString("SHA-256", sum))
+
+        return service.call("auth", "auth", loginJSON.toString())
+    }
+
+
+    fun preAuth(login : String) : Single<Response<ResponseBody>>{
+        val retrofit : Retrofit = Retrofit.Builder()
+                .baseUrl("http://randomus.gq:8000/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
 
         val service : TestInterface = retrofit.create(TestInterface::class.java)
 
-        val login : JsonObject = JsonObject()
-        login.addProperty("login","vova")
+        val loginJSON = JsonObject()
+        loginJSON.addProperty("login", login)
 
 
-        return service.call("auth", "preAuth", login.toString())
+        return service.call("auth", "preAuth", loginJSON.toString())
     }
 
 
@@ -52,6 +74,24 @@ class Core {
 
 
         val service : TestInterface = retrofit.create(TestInterface::class.java)
-        return service.call("event", "call")
+        return service.call("event", "getList")
     }
+
+
+    private fun hashString(type: String, input: String): String {
+        val HEX_CHARS = "0123456789ABCDEF"
+        val bytes = MessageDigest
+                .getInstance(type)
+                .digest(input.toByteArray())
+        val result = StringBuilder(bytes.size * 2)
+
+        bytes.forEach {
+            val i = it.toInt()
+            result.append(HEX_CHARS[i shr 4 and 0x0f])
+            result.append(HEX_CHARS[i and 0x0f])
+        }
+
+        return result.toString()
+    }
+
 }
--- src/main/kotlin/com/focuspoint/capomap/Main.kt
+++ src/main/kotlin/com/focuspoint/capomap/Main.kt
@@ -1,7 +1,25 @@
 package com.focuspoint.capomap
 
 import com.focuspoint.Core
+import com.google.gson.JsonParser
 
 fun main(args: Array<String>) {
-    println(Core().auth().blockingGet())
+
+    val login = "admin"
+    val response = Core().preAuth(login).blockingGet()
+
+    println(response)
+    val obj = JsonParser().parse(response.body()?.string()).asJsonObject
+    val key = obj.getAsJsonObject("Result").getAsJsonPrimitive("key")
+
+    val authResponse = Core().auth(login, "HpX988y61", key.asString).blockingGet()
+
+    val eventResponse = Core().getList().blockingGet()
+
+
+
+
+    println(authResponse.body()?.string())
+    println(eventResponse.body()?.string())
+
 }
